a4dc02da3e15442397738177d5bdd379eb6abcd3
RodrigoBMmoreira <digomellomoreira@gmail.com>
2025-04-02 11:48:26 -0300

    tarefa feita

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const barbearia = {
         { id: 4, tipo: "Moicano", valor: 10 },
         { id: 5, tipo: "Razor part", valor: 5 },
     ],
-  
+
     barbas: [
         { id: 1, tipo: "Capitão Jack", valor: 20 },
         { id: 2, tipo: "Van Dyke", valor: 20 },
@@ -16,30 +16,72 @@ const barbearia = {
     ],
     estaAberto: true,
 };
-  
-  
+
+
 function buscaCortePorId(id) {
+    for(let i=0;i<barbearia.cortes.length;i++){
+        if(id===barbearia.cortes[i].id){
+            return barbearia.cortes[i]
+        }
+    }
+    return "Corte não encontrado"
 }
 
+
 function buscaBarbaPorId(id) {
+    for(let i=0;i<barbearia.barbas.length;i++){
+        if(id===barbearia.barbas[i].id){
+            return barbearia.barbas[i]
+        }
+    }
+    return "Barba não encontrada"
 }
 
 function verificaStatusBarbearia() {
+    if(barbearia.estaAberto===true){
+        return "Estamos abertos"
+    }
+    return "Estamos fechados"
 }
 
 function retornaTodosCortes() {
+    return barbearia.cortes
 }
 
 function retornaTodasBarbas() {
+    return barbearia.barbas
 }
 
 function criaPedido(nomeCliente, corteId, barbaId) {
+    cut=buscaCortePorId(corteId)
+    beard=buscaBarbaPorId(barbaId)
+    let pedido={
+        nome:nomeCliente,
+        pedidoCorte:cut.tipo,
+        pedidoCortePreco:cut.valor,
+        pedidoBarba:beard.tipo,
+        pedidoBarbaPreco:beard.valor
+    }
+    return pedido
 }
 
 function atualizarServico(lista, id, valor, tipo) {
+    let atualizado = false;
+    for (let i = 0; i < lista.length; i++) {
+        if (lista[i].id === id) {
+            lista[i].tipo = tipo;
+            lista[i].valor = valor;
+            atualizado = true;
+            break;
+        }
+    }
+    return atualizado ? lista : "Serviço não encontrado";
 }
 
+
 function calculaTotal(pedido) {
+    let total=pedido.pedidoBarbaPreco+pedido.pedidoCortePreco
+    return total
 }
-  
-  
+
+
